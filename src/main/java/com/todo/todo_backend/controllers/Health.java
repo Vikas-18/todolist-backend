48d2fefa4d7c78f1e341084cda1bbd09ec035fda
Vikas-18 <vikas2k24nitrr@gmail.com>
2026-02-24 08:52:50 +0530

    cross origin updated

--- src/main/java/com/todo/todo_backend/controllers/Health.java
+++ src/main/java/com/todo/todo_backend/controllers/Health.java
@@ -1,8 +1,9 @@
 package com.todo.todo_backend.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "*")
 @RestController
 public class Health {
     @GetMapping("/health")
--- src/main/java/com/todo/todo_backend/controllers/TaskController.java
+++ src/main/java/com/todo/todo_backend/controllers/TaskController.java
@@ -12,11 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/tasks")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
 public class TaskController {
     @Autowired
     private TaskService taskService;
